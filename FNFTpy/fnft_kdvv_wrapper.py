a098fcbe8ba4f546f1507eb2420dd2c1ec4742b3
xmhk <chmhnk@googlemail.com>
2018-06-20 09:42:53 +0200

    python coding conventions (lowercase variables, spaces, etc)

--- FNFTpy/fnft_kdvv_wrapper.py
+++ FNFTpy/fnft_kdvv_wrapper.py
@@ -1,19 +1,19 @@
 from .typesdef import *
 
 
-def kdvv_wrapper(clib_kdvv_func, D, u, t1, t2, M, xi1, xi2,
-                 K, options):
+def kdvv_wrapper(clib_kdvv_func, d, u, t1, t2, m, xi1, xi2,
+                 k, options):
     """
-    FNFT(KDVV) wraps the python input and returns the result from libFNFT
+    Wraps the python input and returns the result from libFNFT's fnft_kdvv
     Parameters:
     ----------
         clib_kdvv_func : handle of the c function imported via ctypes
-        D : number of samples	     
+        d : number of samples
         u : numpy array holding the samples of the field to be analyzed
         t1, t2  : time positions of the first and the last sample
-        M : number of values for the continuous spectrum to calculate
+        m : number of values for the continuous spectrum to calculate
         xi1, xi2 : min and max frequency for the continuous spectrum        
-        K : maximum number of bound states to calculate
+        k : maximum number of bound states to calculate
         options : options for nsev as nsev_options_struct
     Returns:
     ----------
@@ -22,48 +22,48 @@ def kdvv_wrapper(clib_kdvv_func, D, u, t1, t2, M, xi1, xi2,
         contspec : continuous spectrum        
     """
     clib_kdvv_func.restype = ctypes_int
-    KDVV_D = ctypes_uint(D)
-    KDVV_u = np.zeros(KDVV_D.value, dtype=numpy_complex)
-    KDVV_u[:] = u[:] + 0.0j
-    KDVV_T = np.zeros(2, dtype=numpy_double)
-    KDVV_T[0] = t1
-    KDVV_T[1] = t2
-    KDVV_M = ctypes_uint(M)
-    KDVV_contspec = np.zeros(M, dtype=numpy_complex)
-    KDVV_XI = np.zeros(2, dtype=numpy_double)
-    KDVV_XI[0] = xi1
-    KDVV_XI[1] = xi2
-    # KDVV_K = ctypes_uint(K)
+    kdvv_d = ctypes_uint(d)
+    kdvv_u = np.zeros(kdvv_d.value, dtype=numpy_complex)
+    kdvv_u[:] = u[:] + 0.0j
+    kdvv_t = np.zeros(2, dtype=numpy_double)
+    kdvv_t[0] = t1
+    kdvv_t[1] = t2
+    kdvv_m = ctypes_uint(m)
+    kdvv_contspec = np.zeros(m, dtype=numpy_complex)
+    kdvv_xi = np.zeros(2, dtype=numpy_double)
+    kdvv_xi[0] = xi1
+    kdvv_xi[1] = xi2
+    # kdvv_k = ctypes_uint(k)
     # bound states -> will stay empty until implemented
-    # KDVV_boundstates = np.zeros(K,dtype=numpy_complex)
+    # kdvv_boundstates = np.zeros(k,dtype=numpy_complex)
     # discrete spectrum -> will stay empty until implemented
-    # KDVV_discspec = np.zeros(K,dtype=numpy_complex)
-    KDVV_nullptr = ctypes.POINTER(ctypes.c_int)()
+    # kdvv_discspec = np.zeros(k,dtype=numpy_complex)
+    kdvv_nullptr = ctypes.POINTER(ctypes.c_int)()
     clib_kdvv_func.argtypes = [
-        type(KDVV_D),  # D
+        type(kdvv_d),  # d
         np.ctypeslib.ndpointer(dtype=numpy_complex,
                                ndim=1, flags='C'),  # u
         np.ctypeslib.ndpointer(dtype=ctypes_double,
-                               ndim=1, flags='C'),  # T
-        type(KDVV_M),  # M
+                               ndim=1, flags='C'),  # t
+        type(kdvv_m),  # m
         np.ctypeslib.ndpointer(dtype=numpy_complex,
                                ndim=1, flags='C'),  # contspec
         np.ctypeslib.ndpointer(dtype=ctypes_double,
                                ndim=1, flags='C'),  # xi
-        type(KDVV_nullptr),  # K_ptr
-        type(KDVV_nullptr),  # boundstates
-        type(KDVV_nullptr),  # normconsts res
+        type(kdvv_nullptr),  # k_ptr
+        type(kdvv_nullptr),  # boundstates
+        type(kdvv_nullptr),  # normconsts res
         ctypes.POINTER(kdvv_options_struct)]  # options ptr
     rv = clib_kdvv_func(
-        KDVV_D,
-        KDVV_u,
-        KDVV_T,
-        KDVV_M,
-        KDVV_contspec,
-        KDVV_XI,
-        KDVV_nullptr,
-        KDVV_nullptr,
-        KDVV_nullptr,
+        kdvv_d,
+        kdvv_u,
+        kdvv_t,
+        kdvv_m,
+        kdvv_contspec,
+        kdvv_xi,
+        kdvv_nullptr,
+        kdvv_nullptr,
+        kdvv_nullptr,
         ctypes.byref(options))
-    rdict = {'return_value': rv, 'contspec': KDVV_contspec}
+    rdict = {'return_value': rv, 'contspec': kdvv_contspec}
     return rdict
