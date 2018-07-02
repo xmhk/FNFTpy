d6557163730b69b38e194403876782b9a8147d7c
xmhk <chmhnk@googlemail.com>
2018-07-02 23:31:31 +0200

    cases in examples, tests

--- test.py
+++ test.py
@@ -34,15 +34,15 @@ def nsevtest():
 
 def kdvvexample():
     print("KDVV example")
-    d = 256
-    tvec = np.linspace(-1, 1, d)
-    q = np.zeros(d, dtype=np.complex128)
+    D = 256
+    tvec = np.linspace(-1, 1, D)
+    q = np.zeros(D, dtype=np.complex128)
     q[:] = 2.0 + 0.0j
     Xi1 = -2
     Xi2 = 2
-    m = 8
-    Xivec = np.linspace(Xi1, Xi2, m)
-    res = kdvv(q, tvec, m, Xi1=Xi1, Xi2=Xi2, dis=15)
+    M = 8
+    Xivec = np.linspace(Xi1, Xi2, M)
+    res = kdvv(q, tvec, M, Xi1=Xi1, Xi2=Xi2, dis=15)
     print("FNFT return value: %d" % res['return_value'])
     for i in range(len(res['contspec'])):
         print("%d. Xi=%.4f   %.6f  %.6fj" % (i, Xivec[i], np.real(res['contspec'][i]), np.imag(res['contspec'][i])))
@@ -50,13 +50,13 @@ def kdvvexample():
 
 def nsepexample():
     print("NSEP example")
-    d = 256
-    dt = 2 * np.pi / d
-    tvec = np.arange(d) * dt
+    D = 256
+    dt = 2 * np.pi / D
+    tvec = np.arange(D) * dt
     q = np.exp(2.0j * tvec)
     res = nsep(q, 0, 2 * np.pi, bb=[-2, 2, -2, 2], filt=1)
     print("FNFT return value: %d" % res['return_value'])
-    print("number of samples: %d"%d)
+    print("number of samples: %d"%D)
     print('main spectrum')
     for i in range(res['K']):
         print("%d   %.6f  %.6fj" % (i, np.real(res['main'][i]), np.imag(res['main'][i])))
@@ -67,13 +67,13 @@ def nsepexample():
 
 def nsevexample():
     print("NSEV example")
-    d = 256
-    tvec = np.linspace(-1, 1, d)
+    D = 256
+    tvec = np.linspace(-1, 1, D)
     q = np.zeros(len(tvec), dtype=np.complex128)
     q[:] = 2.0 + 0.0j
-    m = 8
-    res = nsev(q, tvec, M=m, Xi1=-2, Xi2=2, K=d)
-    Xivec = np.linspace(-2, 2, m)
+    M = 8
+    res = nsev(q, tvec, M=M, Xi1=-2, Xi2=2, K=D)
+    Xivec = np.linspace(-2, 2, M)
     print("FNFT return value: %d" % res['return_value'])
     print("continuous spectrum")
     for i in range(len(res['c_ref'])):
