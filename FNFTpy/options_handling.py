bfe4974a3d282c9b4606c2db3fd68b42891f9ce6
xmhk <chmhnk@googlemail.com>
2018-07-05 15:20:18 +0200

    options printing functions

--- FNFTpy/options_handling.py
+++ FNFTpy/options_handling.py
@@ -31,7 +31,7 @@ def print_kdvv_options(opts=None):
     if opts is None:
         opts = fnft_kdvv_default_opts_wrapper()
     print("kdvv options:")
-    print("   dis", opts.discretization)
+    print("  discretization : dis ", opts.discretization)
 
 
 def get_kdvv_options(dis=None):
@@ -101,12 +101,13 @@ def print_nsep_options(opts=None):
     if opts is None:
         opts = fnft_nsep_default_opts_wrapper()
     print("nsep options:")
-    print("    loc", opts.localization)
-    print("    filt", opts.filtering)
-    print("    bb", opts.bounding_box[0], opts.bounding_box[1], opts.bounding_box[2], opts.bounding_box[3])
-    print("    maxev", opts.max_evals)
-    print("    dis", opts.discretization)
-    print("    nf flag", opts.normalization_flag)
+    print("  bound state localization      : loc   ", opts.localization)
+    print("  bound state filtering         : filt  ", opts.filtering)
+    print("  bounding box                  : bb    ", opts.bounding_box[0], opts.bounding_box[1], opts.bounding_box[2], opts.bounding_box[3])
+    print("  maximum number of eigenvalues : max_v ", opts.max_evals)
+    print("  discretization                : dis   " , opts.discretization)
+    print("  normalization                 : nf    ", opts.normalization_flag)
+    print(" ")
 
 
 def get_nsep_options(loc=None, filt=None, bb=None, maxev=None, dis=None, nf=None):
@@ -206,13 +207,14 @@ def print_nsev_options(opts=None):
     if opts is None:
         opts = fnft_nsev_default_opts_wrapper()
     print("nsev options:")
-    print("    bsf", opts.bound_state_filtering)
-    print("    bsl", opts.bound_state_localization)
-    print("    niter", opts.niter)
-    print("    dst", opts.discspec_type)
-    print("    cst", opts.contspec_type)
-    print("    dis", opts.discretization)
-    print("    nf", opts.normalization_flag)
+    print("  bound state filtering    : bsf", opts.bound_state_filtering)
+    print("  bound state localization : bsl", opts.bound_state_localization)
+    print("  number of iteratons      : niter", opts.niter)
+    print("  discrete spectrum type   : dst", opts.discspec_type)
+    print("  continuous spectrum type : cst ", opts.contspec_type)
+    print("  discretization           : dis", opts.discretization)
+    print("  normalization flag       : nf", opts.normalization_flag)
+    print(" ")
 
 
 def get_nsev_options(bsf=None, bsl=None, niter=None, dst=None, cst=None, nf=None, dis=None):
