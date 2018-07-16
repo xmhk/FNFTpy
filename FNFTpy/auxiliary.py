c280779f56c92da2383e66f2d79ae2762604d3aa
xmhk <chmhnk@googlemail.com>
2018-07-16 19:52:01 +0200

    get_lib_path docstring

--- FNFTpy/auxiliary.py
+++ FNFTpy/auxiliary.py
@@ -1,3 +1,33 @@
+"""
+This file is part of FNFTpy.
+FNFTpy provides wrapper functions to interact with FNFT,
+a library for the numerical computation of nonlinear Fourier transforms.
+
+For FNFTpy to work, a copy of FNFT has to be installed.
+For general information, source files and installation of FNFT,
+visit FNFT's github page: https://github.com/FastNFT
+
+For information about setup and usage of FNFTpy see README.md.
+
+FNFTpy is free software; you can redistribute it and/or
+modify it under the terms of the version 2 of the GNU General
+Public License as published by the Free Software Foundation.
+
+FNFTpy is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+Contributors:
+
+Christoph Mahnke, 2018
+
+"""
+
+
 def get_lib_path():
     """Return the path of the FNFT file.
 
@@ -7,9 +37,15 @@ def get_lib_path():
     Returns:
 
         libstring : string holding library path
+
+    Example paths:
+
+        libstr = "C:/Libraries/local/libfnft.dll"  # example for windows
+        libstr = "C:\\Libraries\\local\\libfnft.dll" # windows - with backslash
+        libstr = "/usr/local/lib/libfnft.so"  # example for linux
+
     """
-    libstr = "C:/Libraries/local/devel/libfnft.dll"  # example for windows
-    # libstr = "/usr/local/lib/libfnft.so"  # example for linux
+    libstr = "/usr/local/lib/libfnft-devel.so"  # example for linux
     return libstr
 
 
