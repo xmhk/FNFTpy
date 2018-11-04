258fbf1924a5a438badb4ce132164e9188b9ea1b
xmhk <chmhnk@googlemail.com>
2018-11-04 14:04:53 +0100

    continue to transform test functions into testobj

--- tests/test1.py
+++ tests/test1.py
@@ -1,7 +1,38 @@
+"""
+This file is part of FNFTpy.
+FNFTpy provides wrapper functions to interact with FNFT,
+a library for the numerical computation of nonlinear Fourier transforms.
+
+For FNFTpy to work, a copy of FNFT has to be installed.
+For general information, source files and installation of FNFT,
+visit FNFT's github page: https://github.com/FastNFT
+
+For information about setup and usage of FNFTpy see README.md or documentation.
+
+FNFTpy is free software; you can redistribute it and/or
+modify it under the terms of the version 2 of the GNU General
+Public License as published by the Free Software Foundation.
+
+FNFTpy is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+Contributors:
+
+Christoph Mahnke, 2018
+
+"""
+
 from testfunctions import kdvvexample, \
-    nsepexample, nsevexample
+    nsepexample, nsevexample, nsevinverseexample, nsevinverseexample2
 
 TO = nsepexample(verbose=False)
 TO = kdvvexample(verbose=False)
 TO = nsevexample(verbose=False)
+T0 = nsevinverseexample(verbose=False)
+T0 = nsevinverseexample2(verbose=True)
 print(TO.res)
--- tests/testfunctions/testobj_class.py
+++ tests/testfunctions/testobj_class.py
@@ -1,3 +1,32 @@
+"""
+This file is part of FNFTpy.
+FNFTpy provides wrapper functions to interact with FNFT,
+a library for the numerical computation of nonlinear Fourier transforms.
+
+For FNFTpy to work, a copy of FNFT has to be installed.
+For general information, source files and installation of FNFT,
+visit FNFT's github page: https://github.com/FastNFT
+
+For information about setup and usage of FNFTpy see README.md or documentation.
+
+FNFTpy is free software; you can redistribute it and/or
+modify it under the terms of the version 2 of the GNU General
+Public License as published by the Free Software Foundation.
+
+FNFTpy is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+Contributors:
+
+Christoph Mahnke, 2018
+
+"""
+
 
 class Testobj():
     """Run some sample code, store and print results."""
