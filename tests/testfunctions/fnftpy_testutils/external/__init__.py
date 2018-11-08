3e6faeb85aaa74965410fce4ef28b0e95849def1
xmhk <chmhnk@googlemail.com>
2018-11-08 15:13:58 +0100

    external snippet

--- tests/testfunctions/fnftpy_testutils/external/__init__.py
+++ tests/testfunctions/fnftpy_testutils/external/__init__.py
@@ -1,12 +1,21 @@
 import sys, os
 
-#
-# https://meta.stackexchange.com/questions/272956/a-new-code-license-the-mit-this-time-with-attribution-required?cb=1
-#
-
-
 class HiddenPrints:
-    #  Alexander Chzhen edited Jul 27 2018 at 20:02 https://stackoverflow.com/a/45669280
+    """Allows to temporarily hide sdtout messages, e.g. from print.
+
+    usage:
+            with HiddenPrints():
+                do something
+
+    This code snipped was published (posted) on Stackoverflow:
+
+    Code Author: Alexander Chzhen
+    Date       : Jul 27 2018 at 20:02
+    URL        : https://stackoverflow.com/a/45669280
+
+    Code license not explicitly given, is assumed to be MIT license (see URL:
+    https://meta.stackexchange.com/questions/272956/a-new-code-license-the-mit-this-time-with-attribution-required?cb=1)
+    """
     def __enter__(self):
         self._original_stdout = sys.stdout
         sys.stdout = open(os.devnull, 'w')
