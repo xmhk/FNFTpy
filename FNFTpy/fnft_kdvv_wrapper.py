e53cbe2a0e2d9c886d10b9a544315f70190ee7ec
xmhk <chmhnk@googlemail.com>
2021-09-15 22:31:33 +0200

    kdvv_wraper fix some intendation error

--- FNFTpy/fnft_kdvv_wrapper.py
+++ FNFTpy/fnft_kdvv_wrapper.py
@@ -166,24 +166,24 @@ def kdvv_wrapper(D, u, T1, T2, M, Xi1, Xi2,
         kdvv_bound_states_type = type(ctypes_nullptr)
         kdvv_disc_spec_type = type(ctypes_nullptr)
 
-        #
-        # continuous spectrum -> reflection coefficient and / or a,b
-        #
-        kdvv_cont_spec_type = numpy_complex_arr_ptr
-        if options.contspec_type == 0:
-            # reflection coeff.
-            print(kdvv_M.value)
-            kdvv_cont = np.zeros(kdvv_M.value, dtype=numpy_complex)
-        elif options.contspec_type == 1:
-            # a and b
-            kdvv_cont = np.zeros(2 * kdvv_M.value, dtype=numpy_complex)
-        elif options.contspec_type == 2:
-            # a and b AND reflection coeff.
-            kdvv_cont = np.zeros(3 * kdvv_M.value, dtype=numpy_complex)
-        else:
-            # 3 or any other option: skip continuous spectrum -> pass NULL
-            kdvv_cont = ctypes_nullptr
-            kdvv_cont_spec_type = type(ctypes_nullptr)
+    #
+    # continuous spectrum -> reflection coefficient and / or a,b
+    #
+    kdvv_cont_spec_type = numpy_complex_arr_ptr
+    if options.contspec_type == 0:
+        # reflection coeff.
+        print(kdvv_M.value)
+        kdvv_cont = np.zeros(kdvv_M.value, dtype=numpy_complex)
+    elif options.contspec_type == 1:
+        # a and b
+        kdvv_cont = np.zeros(2 * kdvv_M.value, dtype=numpy_complex)
+    elif options.contspec_type == 2:
+        # a and b AND reflection coeff.
+        kdvv_cont = np.zeros(3 * kdvv_M.value, dtype=numpy_complex)
+    else:
+        # 3 or any other option: skip continuous spectrum -> pass NULL
+        kdvv_cont = ctypes_nullptr
+        kdvv_cont_spec_type = type(ctypes_nullptr)
 
     clib_kdvv_func.argtypes = [
         type(kdvv_D),  # D
