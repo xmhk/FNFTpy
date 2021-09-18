e8d3fa972e89d3a3e2fa5ea931866cd1141c77ee
xmhk <chmhnk@googlemail.com>
2021-09-18 18:31:59 +0200

    skeleton of manakovv_wrapper function

--- FNFTpy/fnft_manakovv_wrapper.py
+++ FNFTpy/fnft_manakovv_wrapper.py
@@ -23,18 +23,36 @@ along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 Contributors:
 
-Christoph Mahnke, 2018 - 2020
+Christoph Mahnke 2021
 
 """
 
-from .auxiliary import get_lib_path, get_fnft_version, print_fnft_version
 
-# import wrapper functions
-from .fnft_kdvv_wrapper import kdvv_wrapper, kdvv
-from .fnft_manakovv_wrapper import manakovv_wrapper
-from .fnft_nsep_wrapper import nsep_wrapper, nsep
-from .fnft_nsev_wrapper import nsev_wrapper, nsev
-from .fnft_nsev_inverse_wrapper import nsev_inverse_xi_wrapper, nsev_inverse_wrapper, nsev_inverse
 from .typesdef import *
-from .options_handling import *
+from .auxiliary import get_lib_path, check_return_code, get_winmode_param
+from .options_handling import get_manakovv_options
 
+#def nsev(q, tvec, Xi1=-2, Xi2=2, M=128, K=128, kappa=1, bsf=None,
+#         bsl=None, niter=None, Dsub=None, dst=None, cst=None, nf=None, dis=None, ref=None,
+#         bound_state_guesses = None):
+
+#def nsev_wrapper(D, q, T1, T2, Xi1, Xi2,
+#                 M, K, kappa, options, bound_state_guesses = None):
+
+#FNFT_INT fnft_manakovv(
+# const FNFT_UINT D,
+# FNFT_COMPLEX const* const q1,
+# FNFT_COMPLEX const* const q2,
+#	FNFT_REAL const* const T,
+#	const FNFT_UINT M,
+#	FNFT_COMPLEX* const contspec,
+#	FNFT_REAL const* const XI,
+#	FNFT_UINT* const K_ptr,
+#	FNFT_COMPLEX* const bound_states,
+#	FNFT_COMPLEX* const normconsts_or_residues,
+#	const FNFT_INT kappa,
+#	fnft_manakovv_opts_t* opts);
+
+def manakovv_wrapper(D, q1, q2, T1, T2, Xi1, Xi2, M, K, kappa  ):
+    a= 1
+    pass
